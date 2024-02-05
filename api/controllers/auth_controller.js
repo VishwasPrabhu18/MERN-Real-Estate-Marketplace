56b417e395a5a8f45aebe5b2bfd1e73ebfef02a8
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-05 21:05:16 +0530

    created a middleware and a function to handle the error

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,7 +1,8 @@
 import User from "../modules/user_module.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { userName, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = new User({ userName, email, password: hashedPassword });
@@ -10,6 +11,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json("User Created Successfully");
     } catch (error) {
-        res.status(500).json(error);
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -21,3 +21,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', router);
 app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => { 
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        sucess: false,
+        statusCode,
+        message,
+    });
+});
