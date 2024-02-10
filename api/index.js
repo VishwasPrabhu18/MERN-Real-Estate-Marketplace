a1157b5dcc2cbd2042e04c209b55525d1ddc24c1
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-10 21:43:03 +0530

    created user updated api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import router from "./routes/user_route.js";
 import authRouter from "./routes/auth_route.js";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
