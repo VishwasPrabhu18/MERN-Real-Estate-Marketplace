4e6d8f7c7d3e9b6ff5741af161d61dd39c9e62ee
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-13 23:47:45 +0530

    completed show listong ui

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -11,7 +11,7 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account!"));
-    
+
     try {
         if (req.body.password) {
             req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -36,19 +36,21 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only delete your own account!"));
-    
-    try { 
+
+    try {
         await User.findByIdAndDelete(req.params.id);
         res.clearCookie("access_token");
         res.status(200).json("User has been deleted...");
-    } catch(error) {
+    } catch (error) {
         next(error);
     }
 };
 
 export const getUserListings = async (req, res, next) => {
-    if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only view your own listings!"));
-    
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "You can only view your own listings!"));
+    }
+
     try {
         const listings = await Listing.find({ userRef: req.params.id });
         res.status(200).json(listings);
