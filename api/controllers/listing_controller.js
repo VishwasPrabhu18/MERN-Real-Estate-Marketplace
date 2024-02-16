788cea188c383db706868210eba567a85258ba1c
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-16 22:48:26 +0530

    completed the update api

--- api/controllers/listing_controller.js
+++ api/controllers/listing_controller.js
@@ -1,6 +1,7 @@
 import Listing from "../modules/listing_module.js";
+import { errorHandler } from "../utils/error.js";
 
-export const createListing = async (req, res, next) => { 
+export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
@@ -9,21 +10,45 @@ export const createListing = async (req, res, next) => {
     }
 };
 
-export const deleteListing = async (req, res, next) => { 
+export const deleteListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
 
     if (!listing) {
-        return res.status(404).json({ message: "Listing not found" });
+        return next(errorHandler(404, "Listing not found"));
     }
 
-    if(req.user.id !== listing.userRef) {
-        return res.status(401).json({ message: "You are not authorized to delete this listing" });
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "You are not authorised to delete this listing"));
     }
 
     try {
         await Listing.findByIdAndDelete(req.params.id);
         return res.status(200).json({ message: "Listing deleted" });
     } catch (error) {
-        next(error);        
+        next(error);
+    }
+};
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found"));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "You are not authorized to update this listing"));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body, {
+                new: true
+            }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
     }
 }
