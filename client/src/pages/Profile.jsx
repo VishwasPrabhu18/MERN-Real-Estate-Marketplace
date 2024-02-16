bd3a2bc49bc0978e8274cd3b988ebd98b05f28aa
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-16 21:09:31 +0530

    completed show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -240,7 +240,7 @@ const Profile = () => {
                   <Link to={`/listing/${currentUser._id}`}>
                     <img src={listing.imageUrls[0]} alt="listing images" className="h-16 w-16 object-contain"/>
                   </Link>
-                  <Link className="text-slate-700 font-semibold  hover:underline truncate" to={`/listing/${listing._id}`}>
+                  <Link className="text-slate-700 font-semibold  hover:underline truncate flex-1" to={`/listing/${listing._id}`}>
                     <p>{listing.name}</p>
                   </Link>
                   <div className="flex flex-col items-center">
