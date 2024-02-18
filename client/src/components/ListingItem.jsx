3c5917be3a33ea23c17f648a14f90e05eaac0bb7
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-02-18 12:54:12 +0530

    update

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -6,7 +6,7 @@ const ListingItem = ({ listing }) => {
   return (
     <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
-        <img src={listing.imageUrls[1]} alt="searchImage" className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300" />
+        <img src={listing.imageUrls[0]} alt="searchImage" className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300" />
         <div className="p-3 flex flex-col gap-2 w-full mt-3">
           <p className="text-lg font-semibold text-slate-700 truncate">{listing.name}</p>
           <div className="flex items-center gap-1">
